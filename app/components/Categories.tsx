28a1c40874dcbee63edc27e5712a0811de53b503
Seng Vengchhuong <leobomo5@gmail.com>
2026-01-19 08:01:34 +0700

    test

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -40,7 +40,7 @@ const Categories: React.FC<CategoriesProps> = ({ selectedCategory, onSelect }) =
         <div
           key={cat.id}
           onClick={() => onSelect(cat.name)}
-          className={`px-4 py-2 text-[16px] font-medium rounded-[5px] cursor-pointer flex items-center justify-center min-h-[50px] w-[50px] ${
+          className={`px-4 py-4 text-[16px] font-medium rounded-[5px] cursor-pointer flex items-center justify-center min-h-[50px] w-[70px] ${
             selectedCategory === cat.name ? "bg-blue-600 text-white" : "bg-gray-500 text-white"
           }`}
         >
