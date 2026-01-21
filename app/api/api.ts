f00d47791f51477013e928c4e9e3fafc68a15ff5
Seng Vengchhuong <leobomo5@gmail.com>
2026-01-21 07:52:06 +0700

    back

--- app/api/api.ts
+++ app/api/api.ts
@@ -5,38 +5,27 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-// Attach token automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  withCredentials: true,
 });
 
 // Add request interceptor for cache busting
-// api.interceptors.request.use((config) => {
-//   // Add timestamp to all GET requests to prevent caching
-//   if (config.method === 'get') {
-//     // Add cache busting parameter
-//     config.params = {
-//       ...config.params,
-//       _t: Date.now(), // Always changing
-//       _rand: Math.random().toString(36).substring(2, 9) // Random string
-//     };
+api.interceptors.request.use((config) => {
+  // Add timestamp to all GET requests to prevent caching
+  if (config.method === 'get') {
+    // Add cache busting parameter
+    config.params = {
+      ...config.params,
+      _t: Date.now(), // Always changing
+      _rand: Math.random().toString(36).substring(2, 9) // Random string
+    };
     
-//     // Add cache control headers
-//     config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-//     config.headers['Pragma'] = 'no-cache';
-//     config.headers['Expires'] = '0';
-//   }
+    // Add cache control headers
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    config.headers['Pragma'] = 'no-cache';
+    config.headers['Expires'] = '0';
+  }
   
-//   return config;
-// });
+  return config;
+});
 
 export default api;
