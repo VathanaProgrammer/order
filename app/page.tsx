e2141fef1b15306dc0fbca14de550ce297e7c247
Seng Vengchhuong <leobomo5@gmail.com>
2026-02-04 16:36:41 +0700

    hmm

--- app/page.tsx
+++ app/page.tsx
@@ -360,8 +360,6 @@ export default function ProductPage() {
         </div>
       </div>
 
-      <h1>Test</h1>
-
       <div className="flex flex-1 overflow-y-auto">
         <Categories 
           selectedCategory={selectedCategory} 
